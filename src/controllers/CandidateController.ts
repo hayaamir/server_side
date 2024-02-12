5c1c0e2293507b72654e4968fd33553db86fa253
haya <hmn7707@gmail.com>
2024-02-12 23:32:01 +0200

    final commit

--- src/controllers/CandidateController.ts
+++ src/controllers/CandidateController.ts
@@ -1,84 +1,110 @@
-import {Request, Response} from 'express';
-import Candidate from '../models/candidate';
-import {CandidateService} from "../services/candidateService";
-
+import { Request, Response } from "express";
+import Candidate from "../models/candidate";
+import { CandidateService } from "../services/candidateService";
 
 export class CandidateController {
-    private candidateService: CandidateService;
+  private candidateService: CandidateService;
 
-    constructor(candidateService: CandidateService) {
-        this.candidateService = candidateService;
+  constructor(candidateService: CandidateService) {
+    this.candidateService = candidateService;
+  }
+
+  async createCandidate(req: Request, res: Response): Promise<void> {
+    const candidateBody = req.body;
+    const candidate = new Candidate(
+      candidateBody.id,
+      candidateBody.user_id,
+      candidateBody.first_name,
+      candidateBody.last_name,
+      candidateBody.gender,
+      candidateBody.age,
+      candidateBody.address,
+      candidateBody.current_job,
+      candidateBody.pasts_occupations,
+      candidateBody.parents,
+      candidateBody.siblings,
+      candidateBody.height,
+      candidateBody.remarks,
+      candidateBody.photos,
+      candidateBody.phone
+    );
+    try {
+      await this.candidateService.createCandidate(candidate);
+      res.status(201).send({ message: `Candidate created successfully` });
+    } catch (error) {
+      res.status(400).send((error as Error).message);
     }
+  }
 
-    async createCandidate(req: Request, res: Response): Promise<void> {
-        const candidateBody = req.body;
-        const candidate = new Candidate(
-            candidateBody.id,
-            candidateBody.user_id,
-            candidateBody.first_name,
-            candidateBody.last_name,
-            candidateBody.gender,
-            candidateBody.age,
-            candidateBody.address,
-            candidateBody.current_job,
-            candidateBody.pasts_occupations,
-            candidateBody.parents,
-            candidateBody.siblings,
-            candidateBody.height,
-            candidateBody.remarks,
-            candidateBody.photos,
-            candidateBody.phone
+  async getCandidate(req: Request, res: Response): Promise<void> {
+    const candidateId = +req.params.id;
+    try {
+      const candidate = await this.candidateService.getCandidate(candidateId);
+      res.status(200).send(candidate);
+    } catch (error) {
+      res.status(400).send((error as Error).message);
+    }
+  }
+
+  async updateCandidate(req: Request, res: Response): Promise<void> {
+    const token = req.headers["authorization"];
+
+    if (token === "null") {
+      res.status(401).json({ message: "you are not authenticated!!!" });
+    } else {
+      const candidateId = +req.params.id;
+      const updatedCandidateData = req.body;
+
+      try {
+        await this.candidateService.updateCandidate(
+          candidateId,
+          updatedCandidateData
         );
-        try {
-            await this.candidateService.createCandidate(candidate);
-            res.status(201).send({message: `Candidate created successfully`});
-        } catch (error) {
-            res.status(400).send((error as Error).message);
-        }
+        res.status(200).send({ message: "Candidate updated successfully" });
+      } catch (error) {
+        res.status(400).send((error as Error).message);
+      }
     }
+  }
 
-    async getCandidate(req: Request, res: Response): Promise<void> {
-        const candidateId = +req.params.id;
-        try {
-            const candidate = await this.candidateService.getCandidate(candidateId);
-            res.status(200).send(candidate);
-        } catch (error) {
-            res.status(400).send((error as Error).message);
-        }
+  async deleteCandidate(req: Request, res: Response): Promise<void> {
+    const token = req.headers["authorization"];
+
+    if (token === "null") {
+      res.status(401).json({ message: "you are not authenticated!!!" });
+    } else {
+      const candidateId = +req.params.id;
+      try {
+        await this.candidateService.deleteCandidate(candidateId);
+        res
+          .status(200)
+          .send({ message: `Post ${candidateId} deleted successfully` });
+      } catch (error) {
+        res.status(400).send((error as Error).message);
+      }
     }
+  }
 
-    async updateCandidate(req: Request, res: Response): Promise<void> {
-        const candidateId = +req.params.id;
-        const updatedCandidateData = req.body;
+  async getCandidates(
+    req: Request<
+      {},
+      {},
+      {},
+      { page: number; size: number; filterText?: string }
+    >,
+    res: Response
+  ) {
+    const { page, size, filterText } = req.query;
 
-        try {
-            await this.candidateService.updateCandidate(candidateId, updatedCandidateData);
-            res.status(200).send({message: 'Candidate updated successfully'});
-        } catch (error) {
-            res.status(400).send((error as Error).message);
-        }
+    try {
+      const candidates = await this.candidateService.getCandidates(
+        +page || 0,
+        +size || 4,
+        filterText
+      );
+      res.status(200).send(candidates);
+    } catch (error) {
+      res.status(400).send((error as Error).message);
     }
-
-    async deleteCandidate(req: Request, res: Response): Promise<void> {
-        const candidateId = +req.params.id;
-        try {
-            await this.candidateService.deleteCandidate(candidateId);
-            res.status(200).send({message: `Post ${candidateId} deleted successfully`});
-        } catch (error) {
-            res.status(400).send((error as Error).message);
-        }
-    }
-
-    async getCandidates(req: Request<{}, {}, {}, { page: number, size: number, filterText?: string }>, res: Response) {
-        const {page, size, filterText } = req.query
-
-        try {
-            const candidates = await this.candidateService.getCandidates(+page || 0, +size || 4, filterText);
-            res.status(200).send(candidates);
-        } catch (error) {
-            res.status(400).send((error as Error).message);
-        }
-    }
-
-
+  }
 }
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,52 +1,55 @@
-import { Request, Response } from 'express';
-import User from '../models/user';
+import { Request, Response } from "express";
+import User from "../models/user";
 import { UserService } from "../services/userService";
 
+const users = ["חיה", "מני", "נועם"];
+
 export class UserController {
-    private userService = new UserService();
+  private userService = new UserService();
 
-    async addUser(req: Request, res: Response) {
-        const userData = req.body;
+  async addUser(req: Request, res: Response) {
+    const userData = req.body;
 
-        try {
-          const user = await this.userService.addUser(userData.credential);
-          res.status(201).send({
-            message: "Signup was successful",
-            user: user,
-          });
-        } catch (error) {
-          res.status(400).send((error as Error).message);
-        }
+    try {
+      // const user = await this.userService.addUser(userData.credential);
+      res.status(201).send({
+        message: "Signup was successful",
+        user: users[Math.round(Math.random() * users.length)],
+        token: userData,
+      });
+    } catch (error) {
+      res.status(400).send((error as Error).message);
     }
+  }
 
-    // async getUser(req: Request, res: Response) {
-    //     const userId = +req.params.id;
-    //     try {
-    //         const user = await this.userService.getUser(userId);
-    //         res.status(200).send(user);
-    //     } catch(error) {
-    //         res.status(400).send((error as Error).message);
-    //     }
-    // }
+  // async getUser(req: Request, res: Response) {
+  //     const userId = +req.params.id;
+  //     try {
+  //         const user = await this.userService.getUser(userId);
+  //         res.status(200).send(user);
+  //     } catch(error) {
+  //         res.status(400).send((error as Error).message);
+  //     }
+  // }
 
-    // async updateUser(req: Request, res: Response) {
-    //     const userId = +req.params.id;
-    //     const userData = req.body;
-    //     try {
-    //         await this.userService.updateUser(userId, userData);
-    //         res.status(200).send({ message: `User ${userId} updated successfully` });
-    //     } catch(error) {
-    //         res.status(400).send((error as Error).message);
-    //     }
-    // }
+  // async updateUser(req: Request, res: Response) {
+  //     const userId = +req.params.id;
+  //     const userData = req.body;
+  //     try {
+  //         await this.userService.updateUser(userId, userData);
+  //         res.status(200).send({ message: `User ${userId} updated successfully` });
+  //     } catch(error) {
+  //         res.status(400).send((error as Error).message);
+  //     }
+  // }
 
-    // async deleteUser(req: Request, res: Response) {
-    //     const userId = +req.params.id;
-    //     try {
-    //         await this.userService.deleteUser(userId);
-    //         res.status(200).send({ message: `User ${userId} deleted successfully` });
-    //     } catch(error) {
-    //         res.status(400).send((error as Error).message);
-    //     }
-    // }
+  // async deleteUser(req: Request, res: Response) {
+  //     const userId = +req.params.id;
+  //     try {
+  //         await this.userService.deleteUser(userId);
+  //         res.status(200).send({ message: `User ${userId} deleted successfully` });
+  //     } catch(error) {
+  //         res.status(400).send((error as Error).message);
+  //     }
+  // }
 }
